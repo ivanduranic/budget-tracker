53b9bf99fa5ae18a66eea8903c04f9a16ea56fe5
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-20 23:25:09 -0500

    added link

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+const MONGODB_URI =
+  "mongodb+srv://ivanduranic:Wanderlei@1@cluster0.gljok.mongodb.net/Transaction?retryWrites=true&w=majority";
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
