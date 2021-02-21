85565bdeb4d4aca4102f49e812f6db832bbe72df
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-20 20:51:38 -0500

    connected to Heroku

--- server.js
+++ server.js
@@ -15,10 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const MONGODB_URI =
+  "mongodb+srv://dbUser:Wanderlei@1@cluster0.n6f9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
