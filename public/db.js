e4e9ab9e2b500db39555b50ffaa25a6fcdd8e800
Ivan Duranic <ivan.duranic@gmail.com>
2021-02-20 20:00:17 -0500

    created object store

--- public/db.js
+++ public/db.js
@@ -7,3 +7,8 @@ const indexedDB =
 
 let db;
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+  };
